12e6744cb79c3fb48ee38337bc8237757c073dfa
youlay <youlay.hong@gmail.com>
2024-04-04 19:46:54 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# uc-cpp-arimatic" 
+"# Android-Activities-Lifecyle" 
---
+++ arimatic_operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x = 1;
+	int y = 2;
+	int total;
+	cout << " x = " << x;
+	cout << "\ny = " << y;
+	cout << "\nTotal = " << x + y;
+	
+	return 0;
+	
+}
